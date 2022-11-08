234e6a408e48f125494840e72dd855dfcb2300ff
MahimPC111 <mahimpc111@gmail.com>
2022-11-08 10:26:39 +0600

    setup committed

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+
+function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(e => console.error(e))
+
+app.get('/', (req, res) => {
+    res.send('Eat well server is working!!')
+})
+
+app.listen(port, () => {
+    console.log('This server is listening on port:', port);
+})
