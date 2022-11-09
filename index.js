c52d720949f935b26bb54c50e51c10c4c37d4464
MahimPC111 <mahimpc111@gmail.com>
2022-11-09 18:03:05 +0600

    services  data committed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,16 +9,26 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vmux1xo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function run() {
+
+async function run() {
     try {
+        const serviceCollection = client.db('eatWell').collection('services')
 
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
     }
     finally {
 
     }
 }
-run().catch(e => console.error(e))
+run().catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Eat well server is working!!')
