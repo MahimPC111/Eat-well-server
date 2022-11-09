a1f9531390ac9e673ec0f7c51e73b0d549b23b1b
MahimPC111 <mahimpc111@gmail.com>
2022-11-09 23:29:40 +0600

    single service data committed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,7 +12,6 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vmux1xo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
 async function run() {
     try {
         const serviceCollection = client.db('eatWell').collection('services')
@@ -23,12 +22,26 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services)
         })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const serviceDetails = await serviceCollection.findOne(query);
+            res.send(serviceDetails)
+        })
+
+        app.get('/servicesForHome', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services)
+        })
     }
     finally {
 
     }
 }
-run().catch(err => console.log(err))
+run().catch(err => console.log(err.name, err.message))
 
 app.get('/', (req, res) => {
     res.send('Eat well server is working!!')
